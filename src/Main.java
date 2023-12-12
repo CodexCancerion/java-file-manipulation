882e32d5c4523ac071db23fa73b0ad9f256c718f
CodexCancerion <melbertmarafo2022@gmail.com>
2023-12-12 17:58:59 +0800

    Basic Input/Output program

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import java.io.*;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //FILE WRITER
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/text-file"));
+            writer.write("Hello world");
+            writer.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //FILE READER
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/text-file"));
+            System.out.println(reader.readLine());
+            System.out.println(reader.readLine());
+            reader.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
